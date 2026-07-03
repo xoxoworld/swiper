d9f4e991969ce6d31c022ffa38ff403295d9a385
xoxo <sung021125@gmail.com>
2026-07-03 15:47:23 +0900

    등록

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Myslide from "./Myslide";
+
+function App() {
+  return (
+    <>
+      <h1>Swiper js</h1>
+      <Myslide />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Myslide.jsx
@@ -0,0 +1,40 @@
+import { Navigation, Pagination } from "swiper/modules";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import styles from "./Myslide.module.css";
+import { useRef } from "react";
+
+export default () => {
+  const swiperRef = useRef(null); //참조 생성, 초기값 null
+
+  return (
+    <>
+      <Swiper
+        className={styles.myslide}
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation={true}
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={swiper => (swiperRef.current = swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+      <button onClick={() => swiperRef.current.slideNext()}>다음</button>
+    </>
+  );
+};
